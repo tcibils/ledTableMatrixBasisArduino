0d22a72d21a185023fc8a1a89fdcd081673d6ad1
Thomas Cibils <thomas.cibils@gmail.com>
2020-06-29 20:11:01 +0200

    Adding code to read and use the four SNES controller signals

--- Main/Main.ino
+++ Main/Main.ino
@@ -7,7 +7,11 @@
 #include "FastLED.h"
 #include <avr/pgmspace.h>
 
-// LED MATRIX CODE
+
+// ----------------------------------------------------------------------------------------------------
+// ----------------------------------   LED MATRIX CODE   ---------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+
 // This marix is actually composed of four distinct 16x16 LED matrices. Constant below mirror that.
 
 // Total number of LEDs
@@ -41,6 +45,48 @@ const byte Red = 3;
 const byte Green = 4;
 const byte Purple = 5;
 
+
+// ----------------------------------------------------------------------------------------------------
+// -------------------------------   SNES CONTROLLERS CODE   ------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+
+// Defindes bits for each button
+#define BTN_A 256
+#define BTN_B 1
+#define BTN_X 512
+#define BTN_Y 2
+#define BTN_SELECT 4
+#define BTN_START 8
+#define BTN_UP 16
+#define BTN_DOWN 32
+#define BTN_LEFT 64
+#define BTN_RIGHT 128
+#define BTN_L 1024
+#define BTN_R 2048
+#define NO_GAMEPAD 61440
+
+// PIN Mapping for each controller
+static const byte CONTROLLER_ONE_PIN_LATCH = 40;
+static const byte CONTROLLER_ONE_PIN_CLOCK = 42;
+static const byte CONTROLLER_ONE_PIN_DATA = 44;
+
+static const byte CONTROLLER_TWO_PIN_LATCH = 41;
+static const byte CONTROLLER_TWO_PIN_CLOCK = 43;
+static const byte CONTROLLER_TWO_PIN_DATA = 45;
+
+static const byte CONTROLLER_THREE_PIN_LATCH = 48;
+static const byte CONTROLLER_THREE_PIN_CLOCK = 50;
+static const byte CONTROLLER_THREE_PIN_DATA = 52;
+
+static const byte CONTROLLER_FOUR_PIN_LATCH = 49;
+static const byte CONTROLLER_FOUR_PIN_CLOCK = 51;
+static const byte CONTROLLER_FOUR_PIN_DATA = 53;
+
+
+// ----------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+
 // Useful struct
 struct pointOnMatrix {
   byte lineCoordinate;
@@ -63,6 +109,23 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, LEDSTWO_DATA_PIN>(leds[1], SINGLE_NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, LEDSTHREE_DATA_PIN>(leds[2], SINGLE_NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, LEDSFOUR_DATA_PIN>(leds[3], SINGLE_NUM_LEDS);
+
+  // Sets the controllers as inputs or outputs
+  pinMode(CONTROLLER_ONE_PIN_LATCH, OUTPUT);
+  pinMode(CONTROLLER_ONE_PIN_CLOCK, OUTPUT);
+  pinMode(CONTROLLER_ONE_PIN_DATA, INPUT); 
+  
+  pinMode(CONTROLLER_TWO_PIN_LATCH, OUTPUT);
+  pinMode(CONTROLLER_TWO_PIN_CLOCK, OUTPUT);
+  pinMode(CONTROLLER_TWO_PIN_DATA, INPUT); 
+  
+  pinMode(CONTROLLER_THREE_PIN_LATCH, OUTPUT);
+  pinMode(CONTROLLER_THREE_PIN_CLOCK, OUTPUT);
+  pinMode(CONTROLLER_THREE_PIN_DATA, INPUT); 
+  
+  pinMode(CONTROLLER_FOUR_PIN_LATCH, OUTPUT);
+  pinMode(CONTROLLER_FOUR_PIN_CLOCK, OUTPUT);
+  pinMode(CONTROLLER_FOUR_PIN_DATA, INPUT); 
   
 }
 
@@ -74,6 +137,87 @@ if(millis() - lastMillis > 500) {
   lastMillis = millis();
 }
 
+  // The code below currently reads the buttons from controller X, and display them in a digital display
+  // This will allow me to check that the controller work before coding anything else
+  
+  static uint16_t oldBtns = 0;      // Anciennes valeurs des boutons
+  uint16_t btns = getSnesButtons(0); // Valeurs actuelles des boutons
+ 
+  /* Affiche l'état des boutons uniquement en cas de changement */
+  if(oldBtns != btns)
+    oldBtns = btns;
+  else
+    return;
+     
+  /* Détecte la présence (ou non) d'une manette */
+  if(btns & NO_GAMEPAD) {
+    Serial.println(F("No gamepad connected"));
+    return;
+  }
+   
+  /* Affiche l'état de chaque bouton */
+  if(btns & BTN_A)
+    Serial.print(F("A "));
+  else
+    Serial.print(F("- "));
+ 
+  if(btns & BTN_B)
+    Serial.print(F("B "));
+  else
+    Serial.print(F("- "));
+ 
+  if(btns & BTN_X)
+    Serial.print(F("X "));
+  else
+    Serial.print(F("- "));
+ 
+  if(btns & BTN_Y)
+    Serial.print(F("Y "));
+  else
+    Serial.print(F("- "));
+ 
+  if(btns & BTN_SELECT)
+    Serial.print(F("SELECT "));
+  else
+    Serial.print(F("------ "));
+ 
+  if(btns & BTN_START)
+    Serial.print(F("START "));
+  else
+    Serial.print(F("----- "));
+ 
+  if(btns & BTN_UP)
+    Serial.print(F("UP "));
+  else
+    Serial.print(F("-- "));
+ 
+  if(btns & BTN_DOWN)
+    Serial.print(F("DOWN "));
+  else
+    Serial.print(F("---- "));
+ 
+  if(btns & BTN_LEFT)
+    Serial.print(F("LEFT "));
+  else
+    Serial.print(F("---- "));
+ 
+  if(btns & BTN_RIGHT)
+    Serial.print(F("RIGHT "));
+  else
+    Serial.print(F("----- "));
+ 
+  if(btns & BTN_L)
+    Serial.print(F("L "));
+  else
+    Serial.print(F("- "));
+ 
+  if(btns & BTN_R)
+    Serial.println(F("R"));
+  else
+    Serial.println(F("-"));
+     
+  /* Buttton debounce */
+  delay(25);
     
 
   outputDisplay();
@@ -173,4 +317,71 @@ void digitalOutputDisplay() {
       }
     }
   }
+}
+
+/** Retourne l'état de chaque bouton sous la forme d'un entier sur 16 bits. */
+// For a given controller, which should be from 0 to 3 (so one of the four controllers)
+uint16_t getSnesButtons(byte buttonIndex) {
+ 
+  /* 1 bouton = 1 bit */
+  uint16_t value = 0;
+ 
+  /* Capture de l'état courant des boutons */
+  if(buttonIndex == 0) {
+    digitalWrite(CONTROLLER_ONE_PIN_LATCH, HIGH);
+    digitalWrite(CONTROLLER_ONE_PIN_LATCH, LOW);
+  }
+  if(buttonIndex == 1) {
+    digitalWrite(CONTROLLER_TWO_PIN_LATCH, HIGH);
+    digitalWrite(CONTROLLER_TWO_PIN_LATCH, LOW);
+  }
+  if(buttonIndex == 2) {
+    digitalWrite(CONTROLLER_THREE_PIN_LATCH, HIGH);
+    digitalWrite(CONTROLLER_THREE_PIN_LATCH, LOW);
+  }
+  if(buttonIndex == 3) {
+    digitalWrite(CONTROLLER_FOUR_PIN_LATCH, HIGH);
+    digitalWrite(CONTROLLER_FOUR_PIN_LATCH, LOW);
+  }
+ 
+  /* Récupère l'état de chaque bouton (12 bits + 4 bits à "1") */
+  for(byte i = 0; i < 16; ++i) {
+
+    
+    if(buttonIndex == 0) {
+      /* Lit l'état du bouton et décale le bit reçu pour former l'entier sur 16 bits final */
+      value |= digitalRead(CONTROLLER_ONE_PIN_DATA) << i;
+   
+      /* Pulsation du signal d'horloge */
+      digitalWrite(CONTROLLER_ONE_PIN_CLOCK, HIGH);
+      digitalWrite(CONTROLLER_ONE_PIN_CLOCK, LOW);
+    }
+    if(buttonIndex == 1) {
+      /* Lit l'état du bouton et décale le bit reçu pour former l'entier sur 16 bits final */
+      value |= digitalRead(CONTROLLER_TWO_PIN_DATA) << i;
+   
+      /* Pulsation du signal d'horloge */
+      digitalWrite(CONTROLLER_TWO_PIN_CLOCK, HIGH);
+      digitalWrite(CONTROLLER_TWO_PIN_CLOCK, LOW);
+    }
+    if(buttonIndex == 2) {
+      /* Lit l'état du bouton et décale le bit reçu pour former l'entier sur 16 bits final */
+      value |= digitalRead(CONTROLLER_THREE_PIN_DATA) << i;
+   
+      /* Pulsation du signal d'horloge */
+      digitalWrite(CONTROLLER_THREE_PIN_CLOCK, HIGH);
+      digitalWrite(CONTROLLER_THREE_PIN_CLOCK, LOW);
+    }
+    if(buttonIndex == 3) {
+      /* Lit l'état du bouton et décale le bit reçu pour former l'entier sur 16 bits final */
+      value |= digitalRead(CONTROLLER_FOUR_PIN_DATA) << i;
+   
+      /* Pulsation du signal d'horloge */
+      digitalWrite(CONTROLLER_FOUR_PIN_CLOCK, HIGH);
+      digitalWrite(CONTROLLER_FOUR_PIN_CLOCK, LOW);
+    }
+  }
+ 
+  /* Retourne le résultat sous une forme facile à manipuler (bouton appuyé = bit à "1") */
+  return ~value;
 }
